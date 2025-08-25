6f77c4b35f3f9a450655bd65795db12d8351e040
Fralo <leonardo.fralini@gmail.com>
2025-08-25 11:34:02 +0200

    Remove unnecessary comments from Main class

--- src/main/java/com/expensetracker/Main.java
+++ src/main/java/com/expensetracker/Main.java
@@ -3,13 +3,7 @@ package com.expensetracker;
 import com.expensetracker.controllers.MainController;
 import com.expensetracker.db.DBConnection;
 
-/**
- * Simple CLI entry point. Currently, it just verifies the DB connection and
- * then asks which high-level operation the user wants to perform. The actual
- * implementations will be provided incrementally.
- */
 public class Main {
-
     public static void main(String[] args) {
         DBConnection.initialize("expense_tracker.db");
 
